af1a234001dee10b0db8f2b63cac2e8db22aa135
Tristan Geronimo <tristanpgeronimo@gmail.com>
2022-06-13 11:55:25 +0200

    Add files via upload

---
+++ noSQLORM/src/main/java/it/triexercise/noSQLORM/NoSqlormApplication.java
@@ -0,0 +1,13 @@
+package it.triexercise.noSQLORM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoSqlormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoSqlormApplication.class, args);
+	}
+
+}
